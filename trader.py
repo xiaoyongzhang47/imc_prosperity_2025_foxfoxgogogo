29f862965b6b558d5b7c6adc8df088e0ae1897f9
HCPhy <39453889+HCPhy@users.noreply.github.com>
2025-04-09 13:38:39 -0400

    refactor: remove unused trader.py file and update .DS_Store

--- trader.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict, List
-from datamodel import OrderDepth, TradingState, Order
-import collections
-from collections import defaultdict
-import random
-import math
-import copy
-import numpy as np
